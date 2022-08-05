fd7bdf0bf01ae7c28ef6caaad8711bc27e094c49
Marta <102785427+MartaSolarz@users.noreply.github.com>
2022-08-05 12:45:59 +0200

    Update Password_security.py

--- Password_security.py
+++ Password_security.py
@@ -1,54 +1,137 @@
-# Project - 6.04.2022
-# Is your password secure enough?
+# UPDATE: 5/08/2022
 
-password = input('Podaj hasło: ')
+from string import punctuation
+import sys
 
-alpha_lower = []
-alpha_upper = []
-digit = []
-space = []
-other_char = []
+import click
 
-for char in password:
-    if char.isalpha():
-        if char.islower():
-            alpha_lower.append(char)
-        else:
-            alpha_upper.append(char)
-    elif char.isdigit():
-        digit.append(char)
-    elif char.isspace():
-        space.append(char)
+def check_password_lenght(password:str)->tuple:
+    comment = ''
+    if len(password) < 8:
+        flag = False
+        comment = '- Twoje hasło musi składać się z przynajmniej 8 znaków.'
     else:
-        other_char.append(char)
+        flag = True
 
-too_short = len(password) < 8
-no_alpha_lower = alpha_lower == []
-no_alpha_upper = alpha_upper == []
-no_digit = digit == []
-contains_space = space != []
-no_special_char = other_char == []
+    return flag, comment
 
-no_secure_enough = too_short or no_alpha_lower or no_alpha_upper or no_digit or contains_space or no_special_char
 
-report = ''
+def check_whitespace(password:str)->tuple:
+    comment = ''
+    flag = True
+    for char in password:
+        if char.isspace():
+            flag = False
+            comment = '- Twoje hasło NIE może zawierać spacji.'
+            break
+    
+    return flag, comment
 
-if no_secure_enough:
-    report += 'Twoje hasło nie jest wystarczająco bezpieczne.' '\n' 'Rekomendacje: ' '\n'
-    if too_short:
-        report += '- Twoje hasło powinno składać się z przynajmniej 8 znaków.' '\n'
-    if no_alpha_lower:
-        report += '- Twoje hasło powinno zawierać przynajmniej jedną małą literę.' '\n'
-    if no_alpha_upper:
-        report += '- Twoje hasło powinno zawierać przynajmniej jedną wielką literę.' '\n'
-    if no_digit:
-        report += '- Twoje hasło powinno zawierać przynajmniej jedną cyfrę.' '\n'
-    if contains_space:
-        report += '- Twoje hasło NIE może zawierać spacji.' '\n'
-    if no_special_char:
-        report += '- Twoje hasło powinno zawierać przynajmniej jeden znak specjalny.'
-else:
-    report += 'Twoje hasło jest bezpieczne.'
 
-print() 
-print(report)
+def check_upper_char(password:str)->tuple:
+    flag = False
+    for char in password:
+        if char.isupper():
+            flag = True
+            break
+
+    comment = ''
+    if flag == False:
+        comment = '- Twoje hasło musi zawierać conajmniej jedną dużą literę.'
+
+    return flag, comment
+
+
+def check_lower_char(password:str)->tuple:
+    flag = False
+    for char in password:
+        if char.islower():
+            flag = True
+            break
+
+    comment = ''
+    if flag == False:
+        comment = '- Twoje hasło musi zawierać conajmniej jedną małą literę.'
+
+    return flag, comment
+
+
+def check_special_char(password:str)->tuple:
+    flag = False
+    for char in password:
+        if char in punctuation:
+            flag = True
+            break
+    
+    comment = ''
+    if flag == False:
+        comment = '- Twoje hasło musi zawierać conajmniej jeden znak specjalny.'
+
+    return flag, comment
+
+
+def check_digit(password:str)->tuple:
+    flag = False
+    for char in password:
+        if char.isdigit():
+            flag = True
+            break
+    
+    comment = ''
+    if flag == False:
+        comment = '- Twoje hasło musi zawierać conajmniej jedną cyfrę.'
+
+    return flag, comment
+
+
+def ask_for_raport()->None:
+
+    while True:
+        ask_for_raport = input('Czy wyświetlić rekomendacje? [t/n] ')
+        if ask_for_raport.lower() == 't':
+            break
+        elif ask_for_raport.lower() == 'n':
+            sys.exit(1)
+        else:
+            print('Niedozowolona wartość. Podaj [t/n].')
+
+
+def prepare_raport(checking:tuple[bool, str])->None:
+
+    if checking[0] == False:
+        print(checking[1])
+
+
+@click.command()
+@click.argument('password')
+
+def main(password):
+
+    operations_to_check = [
+        check_password_lenght(password), 
+        check_upper_char(password),
+        check_lower_char(password),
+        check_digit(password),
+        check_special_char(password),
+        check_whitespace(password)
+        ]
+    
+    flag = True
+    comment = 'Twoje hasło jest bezpieczne.'
+    for operation in operations_to_check:
+        if operation[0] == False:
+            flag = False
+            comment = 'Twoje hasło nie jest wystarczająco bezpieczne.'
+            break
+    
+    print(comment)
+
+    if flag == False:
+        ask_for_raport()
+
+        for operation in operations_to_check:
+            prepare_raport(operation)
+
+
+if __name__ == '__main__':
+    main()
